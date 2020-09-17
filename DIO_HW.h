662e26c82fc4cfd818a41b8f233a9b3491091207
Ahmed Mahmoud <61949616+AhmedLNiwehy@users.noreply.github.com>
2020-09-17 23:54:55 +0200

    Update DIO_HW.h

--- DIO_HW.h
+++ DIO_HW.h
@@ -9,19 +9,19 @@
 #ifndef DIO_HW_H_
 #define DIO_HW_H_
 
-#define PORTA_REG		(*(volatile uint8 *)0x3B)
+#define PORTA_REG		    (*(volatile uint8 *)0x3B)
 #define DDRA_REG        (*(volatile uint8 *)0x3A)
 #define PINA_REG        (*(volatile uint8 *)0x39)
 
-#define PORTB_REG		(*(volatile uint8 *)0x38)
+#define PORTB_REG		    (*(volatile uint8 *)0x38)
 #define DDRB_REG        (*(volatile uint8 *)0x37)
 #define PINB_REG        (*(volatile uint8 *)0x36)
 
-#define PORTC_REG		(*(volatile uint8 *)0x35)
+#define PORTC_REG		    (*(volatile uint8 *)0x35)
 #define DDRC_REG        (*(volatile uint8 *)0x34)
 #define PINC_REG        (*(volatile uint8 *)0x33)
 
-#define PORTD_REG		(*(volatile uint8 *)0x32)
+#define PORTD_REG		    (*(volatile uint8 *)0x32)
 #define DDRD_REG        (*(volatile uint8 *)0x31)
 #define PIND_REG        (*(volatile uint8 *)0x30)
 
