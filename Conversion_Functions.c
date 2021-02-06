03d6aa1beeb8310adc239afc07ce92f896a47792
AhmedSolimanNagar <77150918+AhmedSolimanNagar@users.noreply.github.com>
2021-02-06 18:48:22 +0200

    Update Conversion_Functions.c
    Add function to convert Real number ex: -1025.258 to string

--- Conversion_Functions.c
+++ Conversion_Functions.c
@@ -61,3 +61,54 @@ void convert_To_String(sint8 str[], sint32 num)
 	
 	str[len] = '\0';
 }
+
+void convert_To_String(sint8 str[], f32 num)
+{
+	sint32 len=0, len_f = 0, sign, i, rem , Number, Number_frac, temp1,tmpFrac;
+    f32 fraction;
+	
+	
+	if(num < 0)
+	{
+		len++;
+		sign = 1;
+		num = -1*num;
+	}
+	temp1 = num;
+	Number = num;
+	fraction = num - temp1;            // get farction of number 
+	tmpFrac = fraction * 10000;
+	Number_frac = tmpFrac;
+	
+	while(temp1 != 0)                 //finding length of the number
+	{
+		len++;
+		temp1 /= 10;
+		
+	}
+	while(tmpFrac != 0)                 //finding length of the fraction number
+	{
+		len_f++;
+		tmpFrac /= 10;
+	}
+	for(i=0; i<len; i++)             //converting numbers to equivalent char
+	{
+		rem = Number % 10;
+		Number = Number / 10;
+		str[len - (i+1)] = rem + '0';
+	}
+	str[i] = '.';
+	for(i=0; i<len_f; i++)             //converting numbers to equivalent char
+	{
+		rem = Number_frac % 10;
+		Number_frac = Number_frac / 10;
+		str[len + len_f+1 - (i+1)] = rem + '0';
+	}
+	
+	if(sign == 1)
+	{
+		str[0] = '-';
+	}
+	
+	str[len + len_f + 1] = '\0';
+}
